f4a264f91b1523ec16de335d08d7b3d64efa70f7
KimUJeon <kinfe_411@naver.com>
2023-07-04 10:06:25 +0900

    4949 균형잡힌 세상 https://www.acmicpc.net/problem/4949 특이사항: 자료구조, 문자열, 스택 난이도: 실버 4

--- Baekjoon/dir_4000/900/4949_균형잡힌세상.py
+++ Baekjoon/dir_4000/900/4949_균형잡힌세상.py
@@ -23,7 +23,13 @@ while True:
                 break
             else:
                 item_stack.pop()
-    if ans:
+    if ans and not item_stack:
         print("yes")
     else:
         print("no")
+
+'''
+처음에 26번 라인에 not item_stack을 빼고 제출해서 왜 틀린건지 이해가 되질 않았는데 질문 게시판을 둘러보다가
+[. ). 와 같은 반례를 입력하는데 자꾸 yes가 나오길래 고민해보니 스택이 비어있어야 모두 짝이 맞는것으로 간주해야 한다는것을
+간과하고 풀이하였다
+'''
