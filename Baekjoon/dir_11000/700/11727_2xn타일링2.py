85f5c441e6cf1f0a264e3eeae64736741bf60cc6
KimUJeon <kinfe_411@naver.com>
2024-03-26 10:09:47 +0900

    11727 2xn 타일링 2 https://www.acmicpc.net/problem/11727 특이사항: 다이나믹 프로그래밍 난이도: 실버 3
    점화식 생각해내기 어렵다

---
+++ Baekjoon/dir_11000/700/11727_2xn타일링2.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+dp = [0] * 1001
+
+dp[1] = 1
+dp[2] = 3
+
+for idx in range(3, n + 1):
+    dp[idx] = (dp[idx - 1] + dp[idx - 2] * 2) % 10007
+print(dp[n])
