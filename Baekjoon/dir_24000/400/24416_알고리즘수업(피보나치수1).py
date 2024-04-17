a8dcc394c7f468c41b8dc4cba16f9fb5534817e1
KimUJeon <kinfe_411@naver.com>
2024-04-17 21:55:36 +0900

    24416 알고리즘 수업 - 피보나치 수 1 https://www.acmicpc.net/problem/24416 특이사항: 수학, 다이나믹 프로그래밍 난이도: 브론즈 3

---
+++ Baekjoon/dir_24000/400/24416_알고리즘수업(피보나치수1).py
@@ -0,0 +1,24 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+# cnt_fib = 0
+#
+# def fib(n):
+#     global cnt_fib
+#     if n == 1 or n == 2:
+#         cnt_fib += 1
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+
+def fib(n):
+    f = [0] * (n+1)
+    f[1] = f[2] = 1
+    for i in range(3, n+1):
+        f[i] = f[i-1] + f[i-2]
+    return f[n]
+
+def fibos(n):
+    return n-2
+
+print(fib(n), fibos(n))
