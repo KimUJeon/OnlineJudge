3d5de20bcdea0aaaae3a818bf124969813caccd6
KimUJeon <kinfe_411@naver.com>
2024-04-18 09:51:16 +0900

    Indent 수정

--- Baekjoon/dir_24000/400/24416_알고리즘수업(피보나치수1).py
+++ Baekjoon/dir_24000/400/24416_알고리즘수업(피보나치수1).py
@@ -11,14 +11,17 @@ n = int(sys.stdin.readline().rstrip())
 #     else:
 #         return fib(n-1) + fib(n-2)
 
+
 def fib(n):
-    f = [0] * (n+1)
+    f = [0] * (n + 1)
     f[1] = f[2] = 1
-    for i in range(3, n+1):
-        f[i] = f[i-1] + f[i-2]
+    for i in range(3, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
     return f[n]
 
+
 def fibos(n):
-    return n-2
+    return n - 2
+
 
 print(fib(n), fibos(n))
